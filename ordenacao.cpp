dddcf075c9abee58e0bab4afedf0e24e592549b8
Mateus Miranda de Oliveira <mateusmirandammo@gmail.com>
2025-02-14 16:27:08 -0300

    Update ordenacao.cpp

--- ordenacao.cpp
+++ ordenacao.cpp
@@ -1,17 +1,44 @@
+#include <iostream>
 #include "ordenacao.hpp"
 
+
+/*
+  Função que retorna se um vetor 'a' de 't' elementos 
+  está ordenado não decrescentemente --- Ou seja, não está "maior --> menor"
+
+  Retorno: false Se 'a' não estiver ordenado
+           true caso contrário
+*/
 /* TODO: Implementar função */
 bool ordenado(int a[],  unsigned int t){
     return false;
 }
 
+
+/*
+  Função que ordena o vetor 'a' de forma não decrescente
+  usando o método de ordenação por seleção.
+  A ordenação é feita diretamente no vetor 'a'
+*/
 /* TODO: Implementar função */
 void selecao(int a[], unsigned int t){
 }
 
+
+/*
+  Função que ordena o vetor 'a' de forma não decrescente
+  usando o método de ordenação por inserção.
+  A ordenação é feita diretamente no vetor 'a'
+*/
 /* TODO: Implementar função */
 void insercao(int a[], unsigned int t){
 }
 
+
+/*
+  Função que ordena o vetor 'a' de forma não decrescente
+  usando o método de ordenação merge-sort.
+  A ordenação é feita diretamente no vetor 'a'
+*/
 /* TODO: Implementar função */
 void merge_sort(int a[], unsigned int t);
